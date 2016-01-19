8950636ef7e356ecea91831c56adb5654a06ebe9
Haruka <Haruka@haruchan-mac.local>
2016-01-19 22:39:38 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlideshowApp
+//
+//  Created by Haruka on 2016/01/19.
+//  Copyright © 2016年 Haruka.Shida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
