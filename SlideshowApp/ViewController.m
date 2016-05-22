0283b5d49aafa9815ca15200658f9b74a26f9ea2
Haruka <Haruka@haruchan-mac.local>
2016-05-23 08:11:19 +0900

    画像を変更

--- SlideshowApp/ViewController.m
+++ SlideshowApp/ViewController.m
@@ -10,6 +10,7 @@
 
 @interface ViewController (){
     UIImageView *aImageView;
+    NSInteger countNumber;
 }
 @end
 
@@ -18,13 +19,14 @@
 - (void) viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    countNumber = 0;
     [self setupBackground];
     [self setButton];
 }
 
 -(void) setupBackground{
     aImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 320, 568)];
-    aImageView.image = [UIImage imageNamed:@"image1.JPG"];
+    aImageView.image = [UIImage imageNamed:@"IMG_0105.JPG"];
     [self.view addSubview:aImageView];
 }
 
@@ -33,23 +35,30 @@
     button1.frame = CGRectMake(0,0, 120, 45);
     button1.center = CGPointMake(160, 455);
     [button1 setTitle:@"NEXT" forState:UIControlStateNormal];
-    [button1 addTarget:self action:@selector(gotoNext:)forControlEvents:UIControlEventTouchUpInside];
+    [button1 addTarget:self action:@selector(switchImage:)
+    forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:button1];
 
-    
     UIButton *button2 = [UIButton buttonWithType:UIButtonTypeSystem];
     button2.frame = CGRectMake(0, 0, 120, 45);
     button2.center = CGPointMake(160, 510);
     [button2 setTitle:@"BACK" forState:UIControlStateNormal];
-    [button2 addTarget:self action:@selector(goBack:)forControlEvents:UIControlEventTouchUpInside];
+    [button2 addTarget:self action:@selector(switchImage:)
+    forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:button2];
-}
 
--(void) gotoNext:(id)sender{
-    aImageView.image = [UIImage imageNamed:@"image1.JPG"];
-}
--(void) goBack:(id)sender{
-    aImageView.image = [UIImage imageNamed:@"image2.JPG"];
+ }
+
+-(void) switchImage:(id)sender{
+    countNumber++;
+    if (countNumber==1) {
+        aImageView.image = [UIImage imageNamed:@"IMG_0105.JPG"];
+    }else if (countNumber==2){
+        aImageView.image = [UIImage imageNamed:@"IMG_0107.JPG"];
+    }else if (countNumber==3){
+        aImageView.image = [UIImage imageNamed:@"IMG_0109.JPG"];
+        countNumber = 0;
+    }
 }
 
 - (void)didReceiveMemoryWarning {
